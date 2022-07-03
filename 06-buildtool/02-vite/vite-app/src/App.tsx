212dbc878fb40bd49d460557f4a832bc4e301b9d
Yuka O'oka <oukayuka@ayeate.com>
2022-07-03 18:07:33 +0900

    add chapter 6

---
+++ 06-buildtool/02-vite/vite-app/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+const title = import.meta.env.VITE_APP_TITLE;
+console.dir(import.meta.env);
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{title}</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
