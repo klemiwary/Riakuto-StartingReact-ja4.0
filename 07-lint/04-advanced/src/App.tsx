63a7a3ab032153a25700d37b640a8928fd6beb73
Yuka O'oka <oukayuka@ayeate.com>
2022-07-10 18:14:41 +0900

    add rest of chapter 7 lint

---
+++ 07-lint/04-advanced/src/App.tsx
@@ -0,0 +1,29 @@
+import { FC, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const title = import.meta.env.VITE_APP_TITLE;
+console.dir(import.meta.env);
+
+const App: FC = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <p>{title}</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
